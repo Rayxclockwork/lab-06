818a136eed5d289fcc5325ae5d509f4feac5ea14
RAVEN DELANEY <ravend17@gmail.com>
2019-10-22 16:15:47 -0700

    refactoring for API keys

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const superagent = require('superagent');
 
 const app = express();
 app.use(cors());
@@ -13,24 +14,37 @@ app.get('', (request, response) => {
   response.send('Listening on 3000');
 });
 
-app.get('/location', (request, response) => {
-  try{
-    const city = request.query.data;
-    const locationData = searchLatToLong(city);
-    response.send(locationData);
-  }
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+
+
   catch(error) {
     console.error(error);
 
     response.status(500).send('So sorry, something wrong over here!');
+  };
+
+
+function handleLocation(request, response){
+  try{
+    const location = request.query.data;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?addres=${location}&key=${process.env.GEOCODE_API_KEY}`;
+    
+    superagent.get(url)
+      .then(resultsFromSuperAgent);
+
   }
-});
+  catch (error){
+    console.log(error);
+    response.status(500).send('our bad');
+  }
+}
 
 function searchLatToLong(location) {
   const geoData = require('./data/geo.json');
-  const locationObject = new Locale(location, geoData);
+  const locationObj = new Locale(location, geoData);
 
-  return locationObject;
+  return locationObj;
 }
 
 
@@ -40,39 +54,47 @@ function Locale(city, geoData) {
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
 }
-
-let forecast =[];
-
-app.get('/weather', (request, response) => {
-  try{
-    const weather = request.query.data;
-    const weatherData = searchWeather(weather);
-    response.send(weatherData);
-  }
-  catch(error) {
-    console.error(error);
-
-    response.status(500).send('So sorry, something wrong over here!');
-  }
-});
-
-function searchWeather(location) {
-  const wData = require('./data/darksky.json');
-  const weatherObject = new Weather(location, wData);
-
-  return weatherObject;
-}
-
-function Weather(city, wData) {
-  this.search_query = city;
-  this.forecast = wData.results.hourly.summary;
-  this.time = wData.hourly.time;
-
-  forecast.push(this);
-}
-Weather.prototype.revisedDate = function (time){
-  let date = new Date(time*1000);
-  return date.toDateString();
-};
-
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
+
+//superagent.get('url that we want to visit')
+//.then(response from superagent => {
+// responseFromSuperAgent.bosy
+//})
+//.catch(console.error)
+
+
+
+// let forecast =[];
+
+// app.get('/weather', (request, response) => {
+//   try{
+//     const weather = request.query.data;
+//     const weatherData = searchWeather(weather);
+//     response.send(weatherData);
+//   }
+//   catch(error) {
+//     console.error(error);
+
+//     response.status(500).send('So sorry, something wrong over here!');
+//   }
+// });
+
+// function searchWeather(location) {
+//   const wData = require('./data/darksky.json');
+//   const weatherObject = new Weather(location, wData);
+
+//   return weatherObject;
+// }
+
+// function Weather(city, wData) {
+//   this.search_query = city;
+//   this.forecast = wData.results.hourly.summary;
+//   this.time = wData.hourly.time;
+
+//   forecast.push(this);
+// }
+// Weather.prototype.revisedDate = function (time){
+//   let date = new Date(time*1000);
+//   return date.toDateString();
+// };
+
