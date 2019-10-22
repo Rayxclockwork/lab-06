07d1df713dc8b8e55458bc7fcc144a8ba17573ed
RAVEN DELANEY <ravend17@gmail.com>
2019-10-22 10:44:39 -0700

    send response location

--- server.js
+++ server.js
@@ -17,23 +17,24 @@ app.get('/location', (request, response) => {
   try{
     const city = request.query.data;
     const locationData = searchLatToLong(city);
+    response.send(locationData);
   }
   catch(error) {
     console.error(error);
 
-    response.status(500).send('So sorry, something wrong over here!')
+    response.status(500).send('So sorry, something wrong over here!');
   }
-})
+});
 
 function searchLatToLong(location) {
   const geoData = require('./data/geo.json');
-  const locationObject = new Location(location, geoData);
+  const locationObject = new Locale(location, geoData);
 
   return locationObject;
 }
 
 
-function Location(city, geoData) {
+function Locale(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
