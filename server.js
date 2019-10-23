1a6f4a13c81b6d4ea87719221b4596e67e9fdbb8
RAVEN DELANEY <ravend17@gmail.com>
2019-10-22 17:02:58 -0700

    working on code during class

--- server.js
+++ server.js
@@ -14,27 +14,44 @@ app.get('', (request, response) => {
   response.send('Listening on 3000');
 });
 
+
+//routes
 app.get('/location', handleLocation);
 app.get('/weather', handleWeather);
 
 
-  catch(error) {
-    console.error(error);
+//functions
+let locations = {};
+function handleLocation(request, response){
+  const location = request.quesry.data;
 
-    response.status(500).send('So sorry, something wrong over here!');
-  };
+
+  if (locations[url]){
+    response.send(locations[url]);
+  } else {
+    const url = `https://maps.googlespis.com/maps/api/geocode/json?address=${location}&key=${process.env.GEOCODE_API_KEY}`;
+  }
+}
+
+
 
 
 function handleLocation(request, response){
   try{
     const location = request.query.data;
     const url = `https://maps.googleapis.com/maps/api/geocode/json?addres=${location}&key=${process.env.GEOCODE_API_KEY}`;
-    
+
     superagent.get(url)
-      .then(resultsFromSuperAgent);
+      .then(resultsFromSuperAgent => {
+        const locationOb = new Location(location, resultsFromSuperAgent.body.results[0]);
+
+        locations[name] = locationOb;
+
+        response.status(200).send(locationOb);
+      });
 
   }
-  catch (error){
+  catch (error) {
     console.log(error);
     response.status(500).send('our bad');
   }
@@ -47,14 +64,36 @@ function searchLatToLong(location) {
   return locationObj;
 }
 
-
 function Locale(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
 }
+
+
+function handleWeather(request, response) {
+  console.log(request.query.data);
+
+  const locationObject = request.query.data;
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${locationObject.latitude},${locationObject.longitude}`;
+  superagent.get(url)
+    .then(resultsFromSuperAgent => {
+      console.log(resultsFromSuperAgent);
+
+     const weeklyWeather = resultsFromSuperAgent.body.daily.data.map(day => {
+       return new Weather(day);
+    })
+    response.send(weeklyWeather);
+}
+
+function Weather(obj){
+  this.forecast = obj.summary;
+  this.time = new Date(obj.time*1000);
+});
+
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
+
 
 //superagent.get('url that we want to visit')
 //.then(response from superagent => {
@@ -62,9 +101,6 @@ app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
 //})
 //.catch(console.error)
 
-
-
-// let forecast =[];
 
 // app.get('/weather', (request, response) => {
 //   try{
@@ -97,4 +133,4 @@ app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
 //   let date = new Date(time*1000);
 //   return date.toDateString();
 // };
-
+}
